dd6380f6299dac7a0fcd1016073cfbc46bac10e3
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-09-29 11:04:11 -0300

    ✨feat: add React-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export {};
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+
+      {/* Redireciona para página inicial caso não encontre a rota */}
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
