5ddd27f0c22436cf510c36f555ca61c8f22ad903
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-10-04 11:16:10 -0300

    ✨ feat: add componente FerramentasDeDetalhe

---
+++ src/shared/components/ferramentas-de-detalhe/FerramentasDeDetalhe.tsx
@@ -0,0 +1,40 @@
+import { Box, Button, Divider, Icon, Paper, useTheme } from "@mui/material"
+
+export const FerramentasDeDetalhe: React.FC = () => {
+  const theme = useTheme()
+
+  return (
+    <Box
+      component={Paper}
+      display="flex"
+      alignItems="center"
+      gap={1}
+      height={theme.spacing(5)}
+      marginX={1}
+      padding={1}
+      paddingX={2}
+    >
+      <Button variant="contained" color="primary" disableElevation startIcon={<Icon>save</Icon>}>
+        Salvar
+      </Button>
+      <Button variant="outlined" color="primary" disableElevation startIcon={<Icon>save</Icon>}>
+        Salvar e voltar
+      </Button>
+      <Button variant="outlined" color="primary" disableElevation startIcon={<Icon>delete</Icon>}>
+        Apagar
+      </Button>
+      <Button variant="outlined" color="primary" disableElevation startIcon={<Icon>add</Icon>}>
+        Novo
+      </Button>
+      <Divider variant="middle" orientation="vertical" />
+      <Button
+        variant="outlined"
+        color="primary"
+        disableElevation
+        startIcon={<Icon>keyboard_return</Icon>}
+      >
+        Voltar
+      </Button>
+    </Box>
+  )
+}
