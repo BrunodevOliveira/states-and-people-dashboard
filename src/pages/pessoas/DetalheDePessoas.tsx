0c30992dbd023a4645455254b2006f9bacbfd7aa
BrunodevOliveira <brunodevoliveira@gmail.com>
2022-10-11 11:13:16 -0300

    ✨ feat: add ação apagar  pessoa  na page DetalheDePessoas

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,22 +1,54 @@
 import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { LinearProgress } from "@mui/material";
 
 import { FerramentasDeDetalhe } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 
 export const DetalheDePessoas: React.FC = () => {
   const { id = "nova" } = useParams<"id">(); // Dessa forma obtenho a variável id da rota (tipo o useParams com a var que qeuro extrair da rota)
   const navigate = useNavigate();
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [nome, setNome] = useState("");
+
+  useEffect(() => {
+    if (id !== "nova") {
+      setIsLoading(true);
+
+      PessoasService.getById(Number(id)).then((result) => {
+        setIsLoading(false);
+        if (result instanceof Error) {
+          alert(result.message);
+          navigate("/pessoas");
+        } else {
+          console.log(result);
+          setNome(result.nomeCompleto);
+        }
+      });
+    }
+  }, [id, navigate]);
+
   const handleSave = () => {
     console.log("Save");
   };
-  const handleDelete = () => {
-    console.log("Delete");
+  const handleDelete = (id: number) => {
+    if (window.confirm(`Realmente deseja exlcuir ${nome}?`)) {
+      PessoasService.deleteById(id).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          alert(`Registro de ${nome} apagado com sucesso!`);
+          navigate("/pessoas");
+        }
+      });
+    }
   };
 
   return (
     <LayoutBaseDePagina
-      titulo="Detalhe de pessoa"
+      titulo={id === "nova" ? "Nova pessoa" : `Detalhes de ${nome}`}
       barraDeFerramentas={
         <FerramentasDeDetalhe
           textoBotaoNovo="Nova"
@@ -25,12 +57,13 @@ export const DetalheDePessoas: React.FC = () => {
           mostrarBotaoNovo={id !== "nova"}
           aoClicarEmSalvar={handleSave}
           aoClicarEmSalvarEFechar={handleSave}
-          aoClicarEmApagar={handleDelete}
+          aoClicarEmApagar={() => handleDelete(+id)}
           aoClicarEmVoltar={() => navigate("/pessoas")}
           aoClicarEmNovo={() => navigate("/pessoas/detalhe/nova")}
         />
       }
     >
+      {isLoading && <LinearProgress />}
       <p>DetalheDePessoas {id}</p>
     </LayoutBaseDePagina>
   );
